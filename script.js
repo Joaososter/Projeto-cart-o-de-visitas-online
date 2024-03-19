d59db441b704bb3b2575fc9115f4d66780931c5f
João Soster <joaomartinatti95@gmail.com>
2024-03-19 15:51:33 -0300

    chore: remove switch

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function toogleMode() {
-const html = document.documentElement
 
 
-html.classList.toggle('light')
-}
+// function toggleMode() {
+//   const html = document.documentElement
+//   html.classList.toggle("light")
 
+//   // pegar a tag img
+//   const img = document.querySelector("#profile img")
+//   }
 
-
-
-
+//   /*
